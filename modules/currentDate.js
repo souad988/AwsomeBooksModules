a59cd5a3e8d204d9ca2c1db5ac93316b6a793269
souad988 <59707859+souad988@users.noreply.github.com>
2022-02-15 23:56:16 +0100

    Fix linter errors"

--- modules/currentDate.js
+++ modules/currentDate.js
@@ -1,7 +1,7 @@
-import {DateTime} from '../node_modules/luxon/build/es6/luxon.js';
+import { DateTime } from '../node_modules/luxon/build/es6/luxon.js';
 
-export default function displayCurrentDate(dateContainer) { 
+export default function displayCurrentDate(dateContainer) {
   setInterval(() => {
     dateContainer.innerHTML = DateTime.now().toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
   }, 1000);
- }
+}
